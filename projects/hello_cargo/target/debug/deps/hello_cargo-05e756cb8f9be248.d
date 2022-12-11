0f02dd6a2425769592476f79390edb27109ce2aa
Dannie Hybrid <DannieHybrid@Dannies-MacBook-Air.local>
2022-12-12 00:36:13 +0100

    hello cargo

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-05e756cb8f9be248.d
@@ -0,0 +1,5 @@
+/Users/DannieHybrid/Desktop/projects/hello_cargo/target/debug/deps/hello_cargo-05e756cb8f9be248.rmeta: src/main.rs
+
+/Users/DannieHybrid/Desktop/projects/hello_cargo/target/debug/deps/hello_cargo-05e756cb8f9be248.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/DannieHybrid/Desktop/projects/hello_cargo/target/debug/hello_cargo: /Users/DannieHybrid/Desktop/projects/hello_cargo/src/main.rs
